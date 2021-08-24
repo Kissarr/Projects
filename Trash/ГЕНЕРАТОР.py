5053ac63302f252a016eca52b485bc96550b5c7a
Kissarr <erlin000@mail.ru>
2021-08-25 03:06:48 +0700

    Create ГЕНЕРАТОР.py

---
+++ Trash/ГЕНЕРАТОР.py
@@ -0,0 +1,14 @@
+def createGenerator():
+    mylist = range(3)
+    for i in mylist:
+        yield i * i
+
+
+mygenerator = createGenerator()  # создаём генератор
+print(mygenerator)  # mygenerator является объектом!
+
+for i in mygenerator:
+    print(i)
+
+for i in mygenerator:
+    print(i)
